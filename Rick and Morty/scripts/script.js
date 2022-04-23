edf172177aeb68b40655f897dfba5d7e7474954d
JoaoAlb2021 <joaorenato2015@gmail.com>
2022-04-23 01:00:49 -0500

    Render characters per episode

--- Rick and Morty/scripts/script.js
+++ Rick and Morty/scripts/script.js
@@ -27,31 +27,49 @@ function loadEpisodes(){
 
 
 function showEpisode(element){
+    const boxEpisodes = document.getElementById('episodes')
     element.results
-    .forEach(obj => createEpisodeLink(obj))
+        .map(obj => createEpisodeLink(obj))
+        .forEach(element => boxEpisodes.appendChild(element))
+    // console.log(element.results);
 }
 
 function createEpisodeLink(element){
     const episodes = document.createElement('div')
-    document.getElementById('episodes').appendChild(episodes)
     episodes.innerText = element.name
     episodes.classList.add('episodesLinks')
     episodes.addEventListener("click", () => showContentTitle(element))
-    episodes.addEventListener("click", saludo)
     return episodes
 }
     
 function showContentTitle(element){
-    console.log(element.name);
     const main = document.getElementById('main')
-
     main.innerHTML=
     '<h1 class=titleEpisode>' + element.name + '</h1>'+
-    '<h2 class=subTitleEpisode>' + element.episode + ' | ' + element.air_date+'</h2>'+
-    ('<div id=boxCharacter>hola</div>')
+    '<h2 class=subTitleEpisode>' + element.episode + ' | ' + element.air_date+'</h2>'
+    
+    const characterContainer = document.createElement('div')
+    characterContainer.id = 'characterContainer'
+    main.appendChild(characterContainer)
+    
+    const urlCharacter = element.characters
+    urlCharacter.map((characterLink) => cardCharacter(characterLink)).forEach((element) => characterContainer.appendChild(element))
 }
-function saludo(){
-    console.log('hola');
+
+function cardCharacter(characterLink){
+    const div = document.createElement('div')
+    div.classList.add('cardCharacter')
+    fetch(characterLink)
+        .then(result => result.json())
+        .then(card => characterThumbnail(div, card))
+    return div
+    // console.log(card.image);
+}
+
+function characterThumbnail(div, card){
+    div.innerHTML = `<img class = cardImage src= ${card.image}></img>`+
+    `<h3>${card.name}</h3>`+
+    `<h4>${card.species} | ${card.status}`
 }
 
 
